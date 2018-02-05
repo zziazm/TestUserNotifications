a9e7c645419739cd0668ee858f3b85eb3946e3fa
zziazm <1310726454@qq.com>
2018-02-05 10:43:29 +0800

    Initial Commit

---
+++ TestUserNotifications/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TestUserNotifications
+//
+//  Created by 赵铭 on 2018/2/5.
+//  Copyright © 2018年 zm. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ TestUserNotifications/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  TestUserNotifications
+//
+//  Created by 赵铭 on 2018/2/5.
+//  Copyright © 2018年 zm. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
